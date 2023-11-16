7d3b1e13a0104f69b125ecdd569ddb38faf5422f
Ariouz <Ariouz@users.noreply.github.com>
2023-11-16 13:55:05 +0100

    Initial Commit

---
+++ src/main/java/fr/angelsky/angelskyeconomy/AngelSkyEconomy.java
@@ -0,0 +1,17 @@
+package fr.angelsky.angelskyeconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AngelSkyEconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
